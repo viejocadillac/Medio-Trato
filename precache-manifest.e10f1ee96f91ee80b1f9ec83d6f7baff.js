6fef5ee92f1a0464538358e939afd19275ec1cc7
Mathias Moreira <viejocadillac@gmail.com>
2020-01-24 05:37:31 -0300

    Updates

---
+++ precache-manifest.e10f1ee96f91ee80b1f9ec83d6f7baff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7173ce1655c8697eaa7d36e9be4b43ae",
+    "url": "/Medio-Trato/index.html"
+  },
+  {
+    "revision": "02cab1a643efbe8ace17",
+    "url": "/Medio-Trato/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "68fd0c438811fe834569",
+    "url": "/Medio-Trato/static/css/main.bce68cb7.chunk.css"
+  },
+  {
+    "revision": "02cab1a643efbe8ace17",
+    "url": "/Medio-Trato/static/js/2.23ca9ec1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Medio-Trato/static/js/2.23ca9ec1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68fd0c438811fe834569",
+    "url": "/Medio-Trato/static/js/main.4ce91cb8.chunk.js"
+  },
+  {
+    "revision": "b815e5f62ec8b833d110",
+    "url": "/Medio-Trato/static/js/runtime-main.85f5b308.js"
+  },
+  {
+    "revision": "64d3e995114521df0fa79311ae54022b",
+    "url": "/Medio-Trato/static/media/maletin.64d3e995.svg"
+  }
+]);
